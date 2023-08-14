2117dcc590b3957c40bbdd7cd3f8410f7527e35a
cooderzeng <48429942+cooderzeng@users.noreply.github.com>
2023-08-14 03:22:01 +0000

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports ={
+    "apps": [
+      {
+        "name": "bingo",
+        "script": "npm",
+        "args": "start",
+        "env": {
+          "NODE_ENV": "production",
+          "DEBUG":"bingo"
+        }
+      }
+    ]
+  }
+  
